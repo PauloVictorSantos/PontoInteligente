1ec3f18f85c7007050cafd61f3b174c6a4433c91
PauloVictorSantos <paulovictor494@gmail.com>
2019-10-28 05:34:58 -0300

    atualizando

---
+++ src/main/java/com/paulovictor/pontoInteligente/api/security/services/impl/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.paulovictor.pontoInteligente.api.security.services.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.paulov.pontoInteligente.api.entities.Funcionario;
+import com.paulov.pontoInteligente.api.services.FuncionarioService;
+import com.paulovictor.pontoInteligente.api.security.JwtUserFactory;
+
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
